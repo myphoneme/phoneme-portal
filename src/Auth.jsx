ce673071cd323977a2931bf27d2bbe3321bb25ad
Ronik Vats <ronikvats@gmail.com>
2025-03-26 11:34:17 +0530

    login

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,49 +1,20 @@
-// import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
-// import './Auth.css';
-
-// function Auth() {
-//     return (
-//         <div className="custom-container" >
-//             <SignedIn>
-//                 <div className="flex flex-col items-center gap-4">
-//                     <h1>Welcome, You are signed in!</h1>
-//                     <UserButton />
-//                 </div>
-//             </SignedIn>
-
-//             <SignedOut>
-//                 <SignIn />
-//             </SignedOut>
-//         </div>
-//     );
-// }
-
-// export default Auth;
-
-
-import { SignedIn, SignedOut, SignIn, SignUp, UserButton, useUser } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import './Auth.css';
-import UserProfile from './User';
+import UserProfile from "./User";
 
 function Auth() {
-    const { user } = useUser();
-
     return (
-        <div className="custom-container">
+        <div className="custom-container" >
             <SignedIn>
                 <div className="flex flex-col items-center gap-4">
                     <h1>Welcome, You are signed in!</h1>
-                    <p>Your User ID: {user?.id}</p>
                     <UserButton />
+                    <UserProfile/>
                 </div>
             </SignedIn>
 
             <SignedOut>
-                <UserProfile />
-                <div className="flex flex-col items-center gap-4">
-                    <SignIn routing="path" path="/sign-in" />
-                    <SignUp routing="path" path="/sign-up" />
-                </div>
+                <SignIn />
             </SignedOut>
         </div>
     );
--- src/User.jsx
+++ src/User.jsx
@@ -4,11 +4,10 @@ import { useUser } from "@clerk/clerk-react";
 const UserProfile = () => {
     const { user } = useUser();
 
-    if (!user) {
-        return <p>Loading...</p>;
-    }
+    // if (!user) {
+    //     return <p>Loading...</p>;
+    // }
 
     return <h2>Signed-in User ID: {user.id}</h2>;
 };
-
 export default UserProfile;

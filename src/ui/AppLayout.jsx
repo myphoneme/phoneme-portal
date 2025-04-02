c5f930207c80db5f5aeb8cbeb6ee9940b5317096
Ronik Vats <ronikvats@gmail.com>
2025-04-02 16:28:17 +0530

    Login header/footer removed

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header from "../components/Header"
-import { Outlet } from "react-router-dom"
+import { Outlet , useLocation} from "react-router-dom"
+import { Footer } from '../components/Footer/Footer';
 
 const AppLayout = () => {
+  const location = useLocation();
+  const hideHeaderAndFooter = location.pathname === "/login" || location.pathname === "/signup";
   return (
     <>
       <div>
         {/* <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> */}
-        <Header/>
+        {/* <Header/> */}
+        {!hideHeaderAndFooter && <Header />}
         <Outlet />  
+        {/* <Footer/> */}
+        {!hideHeaderAndFooter && <Footer />}
     </div>
     </>
   )
